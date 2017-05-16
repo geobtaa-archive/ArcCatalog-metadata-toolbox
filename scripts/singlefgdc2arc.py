580cf98b8801c0aa172087c9267fb5cea76aba4c
zcewij <majew030@umn.edu>
2017-05-16 13:05:38 -0500

    Added documentation for ISO imported to Arc tool

--- scripts/singlefgdc2arc.py
+++
@@ -1,6 +0,0 @@
-import arcpy, sys
-file = sys.argv[1]
-dir = arcpy.GetInstallInfo("desktop")["InstallDir"]
-translator = dir + "Metadata/Translator/FGDC2ESRI_ISO.xml"
-outfile = file.replace('.xml','_arc.xml')
-arcpy.ESRITranslator_conversion(file,translator,outfile)
--- scripts/synchronize.py
+++ scripts/synchronize.py
@@ -4,4 +4,4 @@ arcpy.env.workspace = sys.argv[1]
 fcList = arcpy.ListFeatureClasses()
 
 for fc in fcList:
-   arcpy.SynchronizeMetadata_conversion(fc,"ALWAYS")
+   arcpy.SynchronizeMetadata_conversion(fc,"CREATED")
